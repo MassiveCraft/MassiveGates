8c40d24ca4085aa85984349d60438872c4930071
Olof Larsson <olof@sylt.nu>
2013-03-02 16:30:23 +0100

    Req update

--- src/com/massivecraft/massivegates/cmdreq/ReqGateSelected.java
+++ src/com/massivecraft/massivegates/cmdreq/ReqGateSelected.java
@@ -5,28 +5,35 @@ import org.bukkit.command.CommandSender;
 import com.massivecraft.massivegates.GSender;
 import com.massivecraft.massivegates.GSenderColl;
 import com.massivecraft.mcore.cmd.MCommand;
-import com.massivecraft.mcore.cmd.req.IReq;
+import com.massivecraft.mcore.cmd.req.ReqAbstract;
 
-public class ReqGateSelected implements IReq
+public class ReqGateSelected extends ReqAbstract
 {
+	private static final long serialVersionUID = 1L;
+	
+	// -------------------------------------------- //
+	// INSTANCE & CONSTRUCT
+	// -------------------------------------------- //
+	
+	private static ReqGateSelected i = new ReqGateSelected();
+	public static ReqGateSelected get() { return i; }
+	
+	// -------------------------------------------- //
+	// OVERRIDE
+	// -------------------------------------------- //
+	
 	@Override
-	public boolean test(CommandSender sender, MCommand command)
+	public boolean apply(CommandSender sender, MCommand command)
 	{
 		GSender gsender = GSenderColl.i.get(sender);
+		if (gsender == null) return false;
 		return gsender.getSelectedGate() != null;
 	}
-
+	
 	@Override
 	public String createErrorMessage(CommandSender sender, MCommand command)
 	{
-		return "<b>You must select a gate before you "+command.getDesc()+".";
+		return "<b>You must select a gate before you "+(command == null ? "do that" : command.getDesc())+".";
 	}
-	
-	// -------------------------------------------- //
-	// INSTANCE
-	// -------------------------------------------- //
-	
-	protected static ReqGateSelected i = new ReqGateSelected();
-	public static ReqGateSelected get() { return i; }
 	
 }
